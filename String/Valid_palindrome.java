4505eb3f7a683745a58e80834adcfca72834462b
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-05-22 10:16:46 +0530

    Update Valid_palindrome.java

--- String/Valid_palindrome.java
+++ String/Valid_palindrome.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-      s = s.replaceAll("[^A-Za-z0-9]", "");
       
-      s = s.toLowerCase();
-      
-      s.replace(" ", "");
-      
-      int start = 0;
-      int end = s.length() - 1;
-      
-     while(start < end){
-       if(s.charAt(start) != s.charAt(end)){
-         return false;
-       }else{
-         start++;
-         end--;
-       }
-     }
-      
-    return true;
-      
+      int start = 0, end = s.length() - 1;
+        s = s.toLowerCase();
+        while (start < end) {
+            if (!Character.isLetterOrDigit(s.charAt(start)))
+                start++;
+            else if (!Character.isLetterOrDigit(s.charAt(end)))
+                end--;
+            else if(s.charAt(start) != s.charAt(end)) 
+                return false;
+            else {
+                start++; end--;
+            }
+        }
+        return true;
       
     }
 }
