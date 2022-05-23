e44e94e56fbda8167efa8644f55c2ce3fd44ae77
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-05-23 22:18:03 +0530

    Update longest Consecutive sequence.java

--- Array/longest Consecutive sequence.java
+++ Array/longest Consecutive sequence.java
@@ -1,4 +1,12 @@
 class Solution {
+    
+    
+//      Input: nums = [100,4,200,1,3,2]
+//      Output: 4
+//      Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+    
+    
+    
     public int longestConsecutive(int[] nums) {
         
       int n = nums.length;
