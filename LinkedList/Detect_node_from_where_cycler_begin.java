57d5b3aebb38b80e0d080dcd25582e8cd9479f0e
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-06-05 09:43:05 +0530

    Create Detect_node_from_where_cycler_begin.java

---
+++ LinkedList/Detect_node_from_where_cycler_begin.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+
+public class Solution {
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (slow == fast) {
+            //loop exist
+
+            slow = head;
+
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return slow;
+        } else {
+            //loop not exist
+            return null;
+        }
+    }
+}
