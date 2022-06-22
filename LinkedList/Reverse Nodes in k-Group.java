00a4489fd8913a24e2aef1622472baa49cccf85c
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-06-22 13:16:39 +0530

    Create Reverse Nodes in k-Group.java

---
+++ LinkedList/Reverse Nodes in k-Group.java
@@ -0,0 +1,48 @@
+//////////////////////////////////////
+// Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3 
+// Output:  3->2->1->6->5->4->8->7->NULL. 
+//
+// Inputs:   1->2->3->4->5->6->7->8->NULL and k = 5
+// Output:  5->4->3->2->1->8->7->6->NULL.
+//////////////////////////////////////
+
+
+class Solution {
+
+     public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        Stack<ListNode> stackList = new Stack<>();
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+           int c = 0;
+            while (current != null && c < k) {
+                stackList.push(current);
+                current = current.next;
+                c++;
+            }
+
+            while (!stackList.isEmpty()) {
+             
+                if (prev == null) {
+                    prev = stackList.pop();
+                    head = prev;
+                } else {
+                    prev.next = stackList.pop();
+                    prev = prev.next;
+                }
+            }
+            prev.next = null;
+        }
+        return head;
+    }
+}
