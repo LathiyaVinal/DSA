07a4cb06eebf0fa768ad9585e3b3cddeaabda1c9
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-06-22 15:05:50 +0530

    Create Palindrom_linked_list.java

---
+++ LinkedList/Palindrom_linked_list.java
@@ -0,0 +1,59 @@
+class Solution {
+    ListNode left;
+
+    public boolean isPalindrome(ListNode head) {
+        // write your code here
+    
+    
+        return checkIfPalindrome(head);
+    }
+
+    public boolean checkIfPalindrome(ListNode right) {
+        if (right == null) {
+            return true;
+        }
+        boolean out = checkIfPalindrome(right.next);
+
+        if (out == false) {
+            return false;
+        } else if (left.val != right.val) {
+            return false;
+        } else {
+            left = left.next;
+            return true;
+        }
+    }
+}
+
+
+
+//////////// another method with stack 
+
+class Solution {
+    ListNode left;
+
+    public boolean isPalindrome(ListNode head) {
+      
+      Stack<ListNode> listnode = new Stack<>();
+      
+      ListNode current = head;
+      
+      while(current != null){
+        listnode.push(current);
+        current = current.next;
+      }
+      
+      while(!listnode.isEmpty()){
+        
+        ListNode top = listnode.pop();
+                
+        if(head.val != top.val){
+          return false;
+        
+        }else{
+          head = head.next;
+        }
+      }
+      return true;
+    }
+}
