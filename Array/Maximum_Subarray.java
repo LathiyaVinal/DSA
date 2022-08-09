5a924b5d0923b045d454ee57d109e83c09652fbb
Vinal <63045485+LathiyaVinal@users.noreply.github.com>
2022-08-09 11:08:28 +0530

    Update Maximum_Subarray.java

--- Array/Maximum_Subarray.java
+++ Array/Maximum_Subarray.java
@@ -1,21 +1,28 @@
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+    
+// This program will use kadane's algorithm. 
+// if currentSum if negative, then start from current element, else append current element in current sum.
+// and check for maximum of currentsum and overallSum.
+    
 class Solution {
     public int maxSubArray(int[] nums) {
         
-        int sum = 0 ;
-        int max = Integer.MIN_VALUE;
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            sum = sum + nums[i];
-            
-            if(sum > max){
-                max = sum;
+        int cSum = nums[0];
+        int oSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (cSum < 0) {
+                cSum = nums[i];
+            } else {
+                cSum = cSum + nums[i];
             }
-            
-            if(sum < 0){
-                sum = 0;
+
+            if (cSum > oSum) {
+                oSum = cSum;
             }
         }
-        
-        return max;
+        return oSum;
     }
 }
